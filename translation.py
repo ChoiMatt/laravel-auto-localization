a180f9f57fbef9c858a267246b1a3480e07045dd
ChoiMatt <mattchoi722@gmail.com>
2025-09-25 12:09:45 +0800

    fix: update script and grammar library paths for consistency

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
-#!/apps/auto-localization-mc/venv/bin/python3
+#!/apps/translation-script-mc/venv/bin/python3
 import os
 import json
 import re
@@ -15,7 +15,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning, message="int argu
 REMOVED_TEXTS_SUMMARY = []  # List of tuples: (removed_text, file_path)
 
 # --- Configuration ---
-GRAMMAR_LIB_PATH = '/apps/auto-localization-mc/blade-grammar.so'
+GRAMMAR_LIB_PATH = '/apps/translation-script-mc/blade-grammar.so'
 CONFIG_FILE_PATH = os.path.normpath(os.path.join(os.path.dirname(__file__), 'config.json'))
 API_SERVICE_ENDPOINT = 'https://translation-d16.uat.theorigo.com/'
 MAX_SERVICE_ENDPOINT = 'http://localhost:8080/tasks/translation/'
